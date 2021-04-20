b9c2d3ff2e66b6abb53ffab9be3a2ee2246c6a53
teresa.delatorre <teresa.delatorre@kaleidos.net>
2021-04-20 08:58:09 +0200

    Apply license change to MPL based

--- prerender_recache/signals.py
+++ prerender_recache/signals.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021-present Kaleidos Ventures SL
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021-present Kaleidos Ventures SL
+
 
 from setuptools import setup, find_packages
 
@@ -11,8 +16,8 @@ setup(
     keywords = 'taiga, prerender',
     author = 'Jesús Espino García',
     author_email = 'jesus.espino@kaleidos.net',
-    url = 'https://github.com/taigaio/taiga-prerender-recache',
-    license = 'MIT-0',
+    url = 'https://github.com/kaleidos-ventures/taiga-prerender-recache',
+    license = 'MPL-2',
     include_package_data = True,
     packages = find_packages(),
     install_requires=[],
@@ -24,7 +29,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT No Attribution License (MIT-0)',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
